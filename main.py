d5efb4468c72c258315885b07e41ef0647e2c1ad
SuzumiyaKonata <treant@qq.com>
2021-01-04 02:57:14 +0800

    21-01-04

--- main.py
+++ main.py
@@ -13,16 +13,16 @@ MAX_WORKER = 4
 #最大容许错误请求数
 MAX_ERROR_COUNT = 5
 
+#记录访问者数量
+data_int = dict()
+
 #不会被拉黑的状态码
 safe_code = [200]
 #黑名单
-black_mutex = threading.Lock()
 black_list = set()
 #错误请求计数
-error_mutex = threading.Lock()
 error_count = dict()
 #内存池
-mem_pool_mutex = threading.Lock()
 mem_pool = dict()
 
 #从请求中读取文件路径
@@ -40,6 +40,11 @@ def safe_check(file_path):
 def get_time():
     return time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
 
+#构造访问数量response
+def get_visitor_num(_num):
+    res = "<p style=\"color: cornflowerblue;\">---欢迎第{0}位朋友！</p>".format(_num)
+    return res.encode("utf-8")
+
 def get_day():
     return time.localtime().tm_yday
 #处理请求函数
@@ -54,11 +59,26 @@ def handle_connection(client_connection, client_address):
 
     request = client_connection.recv(1024).decode("utf-8")
     method, file_path = read_request(request)
+    code = 200
+
     #默认请求
     if file_path == '/':
         file_path = '/index.html'
+    #
+    if file_path == '/get_visitor_num.html':
+        if file_path in data_int:
+            data_int[file_path] += 1
+        else:
+            data_int[file_path] = 1
+        http_response = get_visitor_num(data_int[file_path])
+        print(current_time + ' ' + str(code) + ' ' + client_address + ' ' + method  + ' ' + file_path)
+        try:
+            client_connection.sendall(http_response)
+        except:
+            print(current_time + ' BrokenPipe ' + client_address + ' ' + method  + ' ' + file_path)
+        client_connection.close()
+        return
 
-    code = 200
     #发送文件
     if safe_check(file_path):
         if file_path in mem_pool:
@@ -69,9 +89,7 @@ def handle_connection(client_connection, client_address):
                 http_response = f.read()
                 f.close()
                 #放入内存池
-                mem_pool_mutex.acquire()
                 mem_pool[file_path] = http_response
-                mem_pool_mutex.release()
             except:
                 code = 404
                 http_response = "HTTP/1.1 404 NOT FOUND".encode("utf-8")
@@ -84,9 +102,7 @@ def handle_connection(client_connection, client_address):
         if client_address in error_count:
             error_count[client_address] += 1
             if error_count[client_address] >= MAX_ERROR_COUNT:
-                black_mutex.acquire()
                 black_list.add(client_address)
-                black_mutex.release()
         else:
             error_count[client_address] = 1
         error_mutex.release()
